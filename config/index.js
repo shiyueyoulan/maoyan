a155c1c5335792e986e7ed31f23613713f8ea3c1
shiyueyoulan <1141454529@qq.com>
2019-07-12 10:43:45 +0800

    maoyan

--- config/index.js
+++ config/index.js
@@ -27,7 +27,6 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
     useEslint: true,
